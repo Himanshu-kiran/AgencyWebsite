d84a47a8d02c7b15b4de4df4b992868c49b58068
Himanshu Kiran <himanshukiran799@gmail.com>
2024-08-08 22:59:12 +0530

    images updated

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Blog = () => {
     const blogs = [
-        { id: 1, title: "Creating Streamlined Safeguarding Processes with OneRen", image: "/src/assets/blog1.png" },
-        { id: 2, title: "What are your safeguarding responsibilities and how can you manage them?", image: "/src/assets/blog2.png" },
-        { id: 3, title: "Revamping the Membership Model with Triathlon Australia", image: "/src/assets/blog3.png" }
+        { id: 1, title: "Creating Streamlined Safeguarding Processes with OneRen", image: "/src/assets/new1.png" },
+        { id: 2, title: "What are your safeguarding responsibilities and how can you manage them?", image: "/src/assets/new2.png" },
+        { id: 3, title: "Revamping the Membership Model with Triathlon Australia", image: "/src/assets/new3.png" }
     ];
 
     return (
